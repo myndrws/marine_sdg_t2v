a99c017f32e7c6f0b239a90afa32d514cfdf9a0c
myndrws <myndrws@gmail.com>
2023-05-19 11:42:33 +0100

    two ways to create visuals figured out

---
+++ create_visualisations.py
@@ -0,0 +1,8 @@
+# create some plotly visuals to embed within the html
+
+import plotly.express as px
+df = px.data.gapminder()
+fig = px.scatter(df, x="gdpPercap", y="lifeExp", animation_frame="year", animation_group="country",
+           size="pop", color="continent", hover_name="country",
+           log_x=True, size_max=55, range_x=[100,100000], range_y=[25,90])
+fig.write_html("testing_visuals.html")
