f9535a1e133a79e44fea2e618f6c76cffaecfafc
myndrws <myndrws@gmail.com>
2023-05-19 17:08:10 +0100

    bubble map of topics created

--- create_visualisations.py
+++ create_visualisations.py
@@ -1,8 +1,10 @@
 # create some plotly visuals to embed within the html
 import numpy as np
 from top2vec import Top2Vec
-import umap.umap_ as umap
-import plotly.express as px
+from umap import UMAP
+from sklearn.preprocessing import normalize
+import plotly.graph_objects as go
+import pickle
 
 model = Top2Vec.load("top2vec_model")
 
@@ -10,20 +12,58 @@ model = Top2Vec.load("top2vec_model")
 # this needs to include all the topic vectors
 # and also the sentence vectors
 # and also the word vectors
-all_vectors = np.vstack(model.topic_vectors,
-                        model.model.docvecs,
-                        model.model.wv.vectors)
-reducer = umap.UMAP(random_state=42)
-embedding = reducer.fit_transform(all_vectors)
+all_vectors = np.vstack([model.topic_vectors,
+                        model.model.dv.vectors,
+                        model.model.wv.vectors])
+all_vectors = normalize(all_vectors)
+all_vector_levels = np.concatenate([np.repeat(0, len(model.topic_vectors)),
+                                   np.repeat(1, len(model.model.dv.vectors)),
+                                   np.repeat(2, len(model.model.wv.vectors))]).reshape(-1, 1)
+reducer = UMAP(random_state=42)
+embeddings = reducer.fit_transform(all_vectors)
+with open('2d_umap_embeddings.pkl', 'wb') as f:
+    pickle.dump(embeddings, f)
 
 # in the first visualisation we display only topic vectors
+# the other points (sentences and words) are greyed out dots,
+# and we can't hover over them
+colours = embeddings[(all_vector_levels == 0).squeeze(), 0] * embeddings[(all_vector_levels == 0).squeeze(), 1]
+sizes = (model.topic_sizes-min(model.topic_sizes))/(max(model.topic_sizes)-min(model.topic_sizes)) * 100
+layout = go.Layout(
+    paper_bgcolor='rgba(0,0,0,0)',
+    plot_bgcolor='rgba(0,0,0,0)'
+)
+fig = go.Figure(layout=layout)
+
+# Add trace
+fig.add_trace(go.Scatter(x=embeddings[(all_vector_levels == 0).squeeze(), 0],
+                         y=embeddings[(all_vector_levels == 0).squeeze(), 1],
+                         mode='markers',
+                         name='topics',
+                         marker=dict(
+                             size=sizes,
+                             color=colours,
+                             colorscale='plasma'
+                         ),
+                         text=[', '.join(wordvec[:5]) for wordvec in model.topic_words],
+                         hoverinfo='text',
+                         ))
+
+fig.update_xaxes(visible=False)
+fig.update_yaxes(visible=False)
+fig.write_html("testing_visuals.html", full_html=False, include_plotlyjs='cdn')
 
 # in the second visualisation we display a single topic vector
 # the sentence vectors for that topic
 # the word vectors for that topic
-
-df = px.data.gapminder()
-fig = px.scatter(df, x="gdpPercap", y="lifeExp", animation_frame="year", animation_group="country",
-           size="pop", color="continent", hover_name="country",
-           log_x=True, size_max=55, range_x=[100,100000], range_y=[25,90])
-fig.write_html("testing_visuals.html")
+fig.add_trace(go.Scatter(x=embeddings[(all_vector_levels != 0).squeeze(), 0],
+                         y=embeddings[(all_vector_levels != 0).squeeze(), 1],
+                         mode='markers',
+                         name='sentences and words',
+                         marker=dict(
+                             size=2,
+                             color='grey',  # set color equal to a variable
+                             opacity=0.5
+                         ),
+                         hoverinfo="skip"
+                         ))
