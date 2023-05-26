b85024593c1d62fa539c8a83080d9b1076b1fb46
myndrws <myndrws@gmail.com>
2023-05-26 13:26:14 +0100

    added more args to top2vec model and split out investigation script

---
+++ investigate_model.py
@@ -0,0 +1,21 @@
+# investigate the trained model
+import numpy as np
+from top2vec import Top2Vec
+
+model = Top2Vec.load("top2vec_model")
+
+# investigate model
+all_topic_sizes, all_topic_nums = model.get_topic_sizes()
+
+keywords = ["marine", "coastal", "seas", "oceans", "restore"]
+marine_topic_nums = []
+for kw in keywords:
+    _, _, _, topic_nums = model.search_topics(keywords=[kw], num_topics=5)
+    marine_topic_nums.extend(topic_nums)
+marine_topic_nums = list(set(marine_topic_nums))
+marine_topic_sizes = np.asarray(all_topic_sizes)[marine_topic_nums]
+marine_topic_words = model.topic_words[marine_topic_nums, :]
+
+words, word_scores = model.similar_words(keywords=["ocean"], keywords_neg=[], num_words=20)
+for word, score in zip(words, word_scores):
+    print(f"{word} {score}")
--- train_model.py
+++ train_model.py
@@ -10,23 +10,17 @@ with open(filename, 'rb') as f:
     sentences_object = pickle.load(f)
 
 # train and save model
+umap_args = {'n_neighbors': 15,
+             'n_components': 5,
+             'metric': 'cosine'}
+hdbscan_args = {'min_cluster_size': 50,
+                'metric': 'euclidean',
+                'cluster_selection_method': 'eom'}
+
 model = Top2Vec(sentences_object.sentences,
                 min_count=20,
-                topic_merge_delta=0.3)
+                topic_merge_delta=0.2,
+                umap_args=umap_args,
+                hdbscan_args=hdbscan_args)
 model.save("top2vec_model")
 
-# investigate model
-all_topic_sizes, all_topic_nums = model.get_topic_sizes()
-
-keywords = ["marine", "coastal", "seas", "oceans", "restore"]
-marine_topic_nums = []
-for kw in keywords:
-    _, _, _, topic_nums = model.search_topics(keywords=[kw], num_topics=5)
-    marine_topic_nums.extend(topic_nums)
-marine_topic_nums = list(set(marine_topic_nums))
-marine_topic_sizes = np.asarray(all_topic_sizes)[marine_topic_nums]
-marine_topic_words = model.topic_words[marine_topic_nums, :]
-
-words, word_scores = model.similar_words(keywords=["ocean"], keywords_neg=[], num_words=20)
-for word, score in zip(words, word_scores):
-    print(f"{word} {score}")
