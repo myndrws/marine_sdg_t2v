283e72e80f7a8421388ef22cc267395234215f86
myndrws <myndrws@gmail.com>
2023-06-02 16:08:53 +0100

    got into dataframe and refactored visualisations

--- create_visualisations.py
+++ create_visualisations.py
@@ -1,5 +1,6 @@
 # create some plotly visuals to embed within the html
 import numpy as np
+import pandas as pd
 from top2vec import Top2Vec
 from umap import UMAP
 from sklearn.preprocessing import normalize
@@ -8,6 +9,7 @@ import plotly
 import pickle
 
 model = Top2Vec.load("top2vec_model")
+marine_topics = [407, 144, 159, 163, 351]
 
 # prepare the data to be visualised
 # this needs to include all the topic vectors
@@ -28,7 +30,15 @@ with open('2d_umap_embeddings.pkl', 'wb') as f:
 # topics only umap
 topics_only_embeds = reducer.fit_transform(model.topic_vectors)
 
+# separate into two data arrays for two different traces
+marine_topic_embeds = topics_only_embeds[marine_topics, :]
+not_marine_mask = np.setdiff1d(list(range(len(topics_only_embeds))), marine_topics)
+rest_topic_embeds = topics_only_embeds[not_marine_mask, :]
+
+########################################################
+
 # in the first visualisation we display only topic vectors
+# and we display ALL of them, brightly coloured
 colours = topics_only_embeds[:, 0] * topics_only_embeds[:, 1]
 sizes = (model.topic_sizes - min(model.topic_sizes)) / (max(model.topic_sizes) - min(model.topic_sizes)) * 100
 layout = go.Layout(
@@ -46,7 +56,7 @@ fig.add_trace(go.Scatter(x=topics_only_embeds[:, 0],
                              color=colours,
                              colorscale='plasma',
                              line=dict(color='black',
-                                       width=2
+                                       width=1
                                        )
                          ),
                          text=[', '.join(wordvec[:5]) for wordvec in model.topic_words],
@@ -55,9 +65,56 @@ fig.add_trace(go.Scatter(x=topics_only_embeds[:, 0],
 
 fig.update_xaxes(visible=False)
 fig.update_yaxes(visible=False)
-fig.write_html("testing_visuals.html", full_html=False, include_plotlyjs='cdn')
+fig.write_html("visual_one.html", full_html=False, include_plotlyjs='cdn')
 
-# in the second visualisation we display a single topic vector
+########################################################################
+
+# in the second visual we turn the key topics blue, grey out the rest, and zoom in on animation
+sizes = (model.topic_sizes - min(model.topic_sizes)) / (max(model.topic_sizes) - min(model.topic_sizes)) * 100
+layout = go.Layout(
+    paper_bgcolor='rgba(0,0,0,0)',
+    plot_bgcolor='rgba(0,0,0,0)'
+)
+fig = go.Figure(layout=layout)
+fig.add_trace(go.Scatter(x=rest_topic_embeds[:, 0],
+                         y=rest_topic_embeds[:, 1],
+                         mode='markers',
+                         name='topics',
+                         marker=dict(
+                             size=sizes[not_marine_mask],
+                             sizemin=5,
+                             color="grey",
+                             opacity=0.3,
+                             line=dict(color='grey',
+                                       width=1
+                                       )
+                         ),
+                         text=[', '.join(wordvec) for wordvec in np.asarray(model.topic_words)[not_marine_mask, :5]],
+                         hoverinfo='text',
+                         ))
+fig.add_trace(go.Scatter(x=marine_topic_embeds[:, 0],
+                         y=marine_topic_embeds[:, 1],
+                         mode='markers',
+                         name='topics',
+                         marker=dict(
+                             size=sizes[marine_topics],
+                             sizemin=5,
+                             opacity=1,
+                             color="#57edf7",
+                             line=dict(color='black',
+                                       width=2
+                                       )
+                         ),
+                         text=[', '.join(wordvec[:5]) for wordvec in np.asarray(model.topic_words)[marine_topics, :5]],
+                         hoverinfo='text',
+                         ))
+fig.update_layout(showlegend=False)
+fig.update_xaxes(visible=False)
+fig.update_yaxes(visible=False)
+fig.write_html("visual_two.html", full_html=False, include_plotlyjs='cdn')
+
+
+# in the third visualisation we display a single topic vector
 # the sentence vectors for that topic
 # the word vectors for that topic
 # try this for one topic now, might build dynamic visual after
@@ -122,4 +179,7 @@ fig.add_trace(go.Scatter(x=np.asarray(embeddings[chosen_topic, 0]),
                          ))
 fig.update_xaxes(visible=False)
 fig.update_yaxes(visible=False)
-fig.write_html("testing_visuals_second_plot.html", full_html=False, include_plotlyjs='cdn')
+fig.write_html("visual_3.html", full_html=False, include_plotlyjs='cdn')
+
+
+#####################################################
--- investigate_model.py
+++ investigate_model.py
@@ -1,21 +1,58 @@
 # investigate the trained model
 import numpy as np
+import pandas as pd
 from top2vec import Top2Vec
+import pickle
+from pprint import pprint
 
+### open things
 model = Top2Vec.load("top2vec_model")
+with open('sentences_from_master_dict_2023-05-05.pkl', 'rb') as f:
+    sentences_object = pickle.load(f)
+with open('master_dict_2023-05-05.pkl', 'rb') as f:
+    master_dict = pickle.load(f)
 
 # investigate model
 all_topic_sizes, all_topic_nums = model.get_topic_sizes()
 
-keywords = ["marine", "coastal", "seas", "oceans"]
-marine_topic_nums = []
-for kw in keywords:
-    _, _, _, topic_nums = model.search_topics(keywords=[kw], num_topics=5)
-    marine_topic_nums.extend(topic_nums)
-marine_topic_nums = list(set(marine_topic_nums))
-marine_topic_sizes = np.asarray(all_topic_sizes)[marine_topic_nums]
-marine_topic_words = model.topic_words[marine_topic_nums, :]
-
+keywords = ["marine", "coastal", "seas", "oceans", "restoration", "health"]
+m_topic_words, m_word_scores, m_topic_scores, m_topic_nums = model.search_topics(keywords=keywords,
+                                                                                 num_topics=5)
 words, word_scores = model.similar_words(keywords=["ocean"], keywords_neg=[], num_words=20)
 for word, score in zip(words, word_scores):
     print(f"{word} {score}")
+
+# create a big dataframe with everything in it
+marine_docs = {}
+for topic in m_topic_nums:
+    topic_size = model.topic_sizes.loc[topic]
+    mdocs, mscores, mids = model.search_documents_by_topic(topic_num=topic, num_docs=topic_size)
+    marine_docs[f"topic_{topic}"] = pd.DataFrame({"Documents": mdocs,
+                                                  "Scores": mscores,
+                                                  "IDs": mids})
+marine_docs_df = pd.concat(marine_docs)
+marine_docs_df["uids"] = np.asarray(sentences_object.uids)[list(marine_docs_df.IDs)]
+uids_split = [uids.split("_") for uids in list(marine_docs_df.uids)]
+marine_docs_df["doc_title"] = [master_dict[uid[0] + "_" + uid[1]]["title"] for uid in uids_split]
+marine_docs_df["doc_date"] = [master_dict[uid[0] + "_" + uid[1]]["updated"] for uid in uids_split]
+marine_docs_df["doc_url"] = [master_dict[uid[0] + "_" + uid[1]]["url"] for uid in uids_split]
+unique_docs = set(marine_docs_df.doc_title)
+
+
+def makeParaFromSentence(all_sentences_object, sentence_uid, n_sentence_either_side):
+    page_id, doc_id, sentence_id = sentence_uid.split("_")
+    para = ""
+    for ind, uid in enumerate(all_sentences_object.uids):
+        if uid.startswith(f"{page_id}_{doc_id}_"):
+            this_doc_id = int(uid.split("_")[2])
+            cond1 = this_doc_id >= (int(sentence_id) - n_sentence_either_side)
+            cond2 = this_doc_id <= (int(sentence_id) + n_sentence_either_side)
+            if cond1 and cond2:
+                para += " " + all_sentences_object.sentences[ind]
+    return para
+
+example_para = makeParaFromSentence(all_sentences_object=sentences_object,
+                                    sentence_uid="29_17_870",
+                                    n_sentence_either_side=3)
+
+pprint(example_para)
